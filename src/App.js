16fbc0509763d4a96c6baca487a7a85153af4238
returnys <return.ys@gmail.com>
2023-06-14 11:50:09 +0900

    feat: todo 목록 전체삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -14,21 +14,28 @@ function App() {
   ];
   // dummy data state 변수
   const [todoData, setTodoData] = useState(dummy);
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
 
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-300">
       <div className="w-4/5 p-6 bg-white rounded-[8px] shadow">
-        <div>
-          <div className="flex justify-between mb-3">
-            <h1 className="text-center w-3/4 text-2xl font-bold text-cyan-500">
-              Firebase Todo-List
-            </h1>
-          </div>
-          {/* 할 일 목록 */}
-          <List todoData={todoData} setTodoData={setTodoData} />
-          {/* 할 일 추가 */}
-          <Form todoData={todoData} setTodoData={setTodoData} />
+        <div className="flex justify-between mb-3">
+          <h1 className="text-center w-3/4 text-2xl font-bold text-cyan-500">
+            Firebase Todo-List
+          </h1>
+          <button
+            className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400 text-[12px]"
+            onClick={handleRemoveClick}
+          >
+            Delete All
+          </button>
         </div>
+        {/* 할 일 목록 */}
+        <List todoData={todoData} setTodoData={setTodoData} />
+        {/* 할 일 추가 */}
+        <Form todoData={todoData} setTodoData={setTodoData} />
       </div>
     </div>
   );
